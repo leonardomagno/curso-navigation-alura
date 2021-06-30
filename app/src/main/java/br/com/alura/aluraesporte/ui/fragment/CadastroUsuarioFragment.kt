5d0840bf3be6332b01b3757ec0277b6918dcfadd
Leonardo Magno Marones Lopes <leonardo.marones@mobicare.com.br>
2021-06-30 16:10:23 -0300

    - aula 4

--- app/src/main/java/br/com/alura/aluraesporte/ui/fragment/CadastroUsuarioFragment.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/fragment/CadastroUsuarioFragment.kt
@@ -6,13 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import br.com.alura.aluraesporte.R
+import br.com.alura.aluraesporte.ui.viewmodel.EstadoAppViewModel
 import kotlinx.android.synthetic.main.cadastro_usuario.*
+import org.koin.android.viewmodel.ext.android.sharedViewModel
 
 class CadastroUsuarioFragment : BaseFragment() {
 
     private val controlador by lazy {
         findNavController()
     }
+
+    private val estadoAppViewModel: EstadoAppViewModel by sharedViewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,6 +32,7 @@ class CadastroUsuarioFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        estadoAppViewModel.temAppBar = false
         cadastro_usuario_botao_cadastrar.setOnClickListener {
             controlador.popBackStack()
         }
